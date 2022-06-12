f2060016430fab8ae647ba7ec2490531a81e4fb2
Jon Martirosyan <jonmartirosyan1988@gmail.com>
2022-06-13 00:04:10 +0800

    first development commit

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom'
 import { Stack } from '@mui/material'
 
 const Navbar = () => {
-  return <div>Navbar</div>
+  return <div> </div>
 }
 
 export default Navbar
